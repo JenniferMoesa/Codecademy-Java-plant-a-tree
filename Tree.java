e196e6ac5da4a59dc099e97e33f1e52a25556370
Jennifer Moesa <82216812+JenniferMoesa@users.noreply.github.com>
2021-07-20 13:52:19 +0200

    Add files via upload

---
+++ Tree.java
@@ -0,0 +1,13 @@
+// Define your tree class in this file!
+public class Tree {
+  public static void main (String[]args){
+    //Introduce self and print tree
+    System.out.println ("Hey there, I'm Jennifer and I'm learning to code in Java!");
+    System.out.println ("Let's plant a tree!");
+    System.out.println("     **      ");
+    System.out.println("    ****     ");
+    System.out.println("   ******    ");
+    System.out.println("  ********   ");
+    System.out.println("     **      ");  
+  }
+}
